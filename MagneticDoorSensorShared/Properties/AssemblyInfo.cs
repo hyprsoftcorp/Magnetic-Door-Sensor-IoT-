f7b28e7da9efb2bbcd1fb5ed23d15cb2fe1a4f53
HalHunt <hblakeslee@gmail.com>
2016-07-27 11:27:59 -0700

    Added application insights to the web api project.  Also updated Nuget packages.

--- MagneticDoorSensorShared/Properties/AssemblyInfo.cs
+++ MagneticDoorSensorShared/Properties/AssemblyInfo.cs
@@ -1,17 +1,15 @@
 ﻿using System.Resources;
 using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("MagneticDoorSensorShared")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("Magnetic Door Sensor Shared Library")]
+[assembly: AssemblyDescription("Magnetic Door Sensor Shared Library")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("MagneticDoorSensorShared")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyCompany("Hyprsoft Corporation")]
+[assembly: AssemblyProduct("Magnetic Door Sensor Shared Library")]
+[assembly: AssemblyCopyright("Copyright © 2016 by Hyprsoft Corporation.  All rights reserved.")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: NeutralResourcesLanguage("en")]
@@ -26,5 +24,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.0.1")]
+[assembly: AssemblyFileVersion("1.0.0.1")]
--- MagneticDoorSensorWebApi/Properties/AssemblyInfo.cs
+++ MagneticDoorSensorWebApi/Properties/AssemblyInfo.cs
@@ -1,16 +1,15 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("MagneticDoorSensorWebApi")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("Magnetic Door Sensor Web Api")]
+[assembly: AssemblyDescription("Magnetic Door Sensor Web Api")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("MagneticDoorSensorWebApi")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyCompany("Hyprsoft Corporation")]
+[assembly: AssemblyProduct("Magnetic Door Sensor Web Api")]
+[assembly: AssemblyCopyright("Copyright © 2016 by Hyprsoft Corporation.  All rights reserved.")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -31,5 +30,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.0.1")]
+[assembly: AssemblyFileVersion("1.0.0.1")]

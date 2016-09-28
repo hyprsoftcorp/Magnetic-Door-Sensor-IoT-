6c77c0717aebf03ae46dcf5c27422833f3e8a674
HalHunt <hblakeslee@gmail.com>
2016-09-28 14:45:14 -0700

    Added some very minimal logging.

--- MagneticDoorSensorBackgroundApp/StartupTask.cs
+++ MagneticDoorSensorBackgroundApp/StartupTask.cs
@@ -3,16 +3,21 @@ using System;
 using Windows.ApplicationModel.Background;
 using Windows.Devices.Gpio;
 using MagneticDoorSensorShared;
+using Windows.Storage;
+using System.Threading.Tasks;
 
 namespace MagneticDoorSensorBackgroundApp
 {
     public sealed class StartupTask : IBackgroundTask
     {
+        private const string DefaultDateTimeFormat = "G";
+        private const string DefaultLogFilename = "error.log";
+
         private BackgroundTaskDeferral _deferral;
         private GpioPin _sensorPin;
         private IHubProxy _hubProxy;
 
-        public void Run(IBackgroundTaskInstance taskInstance)
+        public async void Run(IBackgroundTaskInstance taskInstance)
         {
             _deferral = taskInstance.GetDeferral();
             taskInstance.Canceled += (sender, reason) => _deferral?.Complete();
@@ -22,30 +27,35 @@ namespace MagneticDoorSensorBackgroundApp
                 var controller = GpioController.GetDefault();
                 _sensorPin = controller.OpenPin(18);
                 _sensorPin.SetDriveMode(GpioPinDriveMode.InputPullUp);
-
                 var hubConnection = new HubConnection(Constants.SignalREndPoint);
                 _hubProxy = hubConnection.CreateHubProxy("SensorHub");
-                hubConnection.Start();
+                await hubConnection.Start();
 
-                _sensorPin.ValueChanged += (s, a) =>
+                _sensorPin.ValueChanged += async (s, a) =>
                 {
                     try
                     {
-                        _hubProxy.Invoke("DataChanged", new SensorData
+                        await _hubProxy.Invoke("DataChanged", new SensorData
                         {
                             State = _sensorPin.Read() == GpioPinValue.High ? SensorState.Open : SensorState.Closed
                         });
                     }
                     catch (Exception ex)
                     {
-                        System.Diagnostics.Debug.WriteLine(ex);
+                        await LogAsync(ex.ToString());
                     }
                 };
             }
             catch (Exception ex)
             {
-                System.Diagnostics.Debug.WriteLine(ex);
+                await LogAsync(ex.ToString());
             }
+        }
+
+        private async Task LogAsync(string message)
+        {
+            var logFile = await ApplicationData.Current.LocalFolder.CreateFileAsync(DefaultLogFilename, CreationCollisionOption.OpenIfExists);
+            await FileIO.AppendTextAsync(logFile, $"{DateTime.Now.ToString("MM/dd/yyyy hh:mm:ss")} {message} {Environment.NewLine}");
         }
     }
 }

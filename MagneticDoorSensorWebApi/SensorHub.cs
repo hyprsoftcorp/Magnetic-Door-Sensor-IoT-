ce9f792a566abde79bc36001d7c0d0ae4162cf41
HalHunt <hblakeslee@gmail.com>
2016-10-01 08:26:37 -0700

    Changed the way the most recent sensor data is stored on the server (from static in memory to local json file persistance).  When our IIS worker process recycles the latest sensor data is now persisted.

--- MagneticDoorSensorWebApi/SensorHub.cs
+++ MagneticDoorSensorWebApi/SensorHub.cs
@@ -1,23 +1,33 @@
 ﻿using System.Threading.Tasks;
 using Microsoft.AspNet.SignalR;
 using MagneticDoorSensorShared;
+using System.Web.Hosting;
+using System.IO;
+using Newtonsoft.Json;
 
 namespace MagneticDoorSensorWebApi
 {
     public class SensorHub : Hub
     {
-        // We only have a single door sensor so this static property will be shared by all clients.
-        public static SensorData Data { get; private set; } = new SensorData();
+        private static object _lockObject = new object();
+
+        private string Path { get { return HostingEnvironment.MapPath("~/App_Data/sensordata.json"); } }
 
         public void DataChanged(SensorData data)
         {
-            Data = data;
+            lock (_lockObject)
+                File.WriteAllText(Path, JsonConvert.SerializeObject(data));
             Clients.All.dataChanged(data);
         }
 
         public override Task OnConnected()
         {
-            Clients.Caller.dataChanged(Data);
+            lock (_lockObject)
+            {
+                if (!File.Exists(Path))
+                    File.WriteAllText(Path, JsonConvert.SerializeObject(new SensorData()));
+                Clients.Caller.dataChanged(JsonConvert.DeserializeObject<SensorData>(File.ReadAllText(Path)));
+            }
             return base.OnConnected();
         }
     }
